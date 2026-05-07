4a4ad004663255bf315a0a3843d8e3c9f9dd1d9b
maruthi <maruthi198916@gmail.com>
2026-05-07 17:33:05 +0530

    factorial

--- Hello.js
+++ Hello.js
@@ -1,1 +1,20 @@
 console.log("Hello js");
+
+let a=10;
+console.log(a);
+
+var msg="Hello js how are you";
+console.log(msg);
+
+const pi=3.123;
+console.log(pi);
+
+for(let i=0; i<5;i++){
+    console.log(i);
+}
+
+let fact=1;
+for(let i=1;i<=5;i++){
+    fact*=i;
+}
+console.log(fact);
