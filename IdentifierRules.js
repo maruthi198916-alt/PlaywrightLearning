21bc41020dd98c8ae8143a943f5c93e0490aec87
maruthi <maruthi198916@gmail.com>
2026-05-08 13:08:17 +0530

    Add .gitignore and IdentifierRules.js

---
+++ IdentifierRules.js
@@ -0,0 +1,21 @@
+var Name="Maruthi";
+var name="Maruthi";
+
+var _name="Maruthi";
+var $name="Maruthi";
+
+//var #name="Maruthi";
+
+var firstName="Maruthi";
+var first_name="Maruthi";
+
+var FirstName="Maruthi";
+var First_Name="Maruthi";
+
+var $=10;
+var _ =30;
+
+var ma123="Maruthi";
+
+console.log(Name);
+
