9b99faf01443050ce227a56cb7d39a3b2a617350
maruthi <maruthi198916@gmail.com>
2026-05-08 15:06:07 +0530

    identifiers

---
+++ Comments.js
@@ -0,0 +1,11 @@
+
+//Single line comment
+//let a=10;
+//console.log(a);
+
+
+/* Multi line comments   */
+/*var UserName="maruthi";
+var Password="maruthi123";
+var Security_Question="What is your pet name?";*/
+
---
+++ IdentifiersRules1.js
@@ -0,0 +1,14 @@
+let user_name="maruthi";
+let Username="maruthi";
+let User_Name="maruthi";
+
+const Max_Value=100;
+const Min_Value=0;
+
+let result=true;
+let Result=false;
+
+let array=[];
+let nCount=10;
+let n_count=10; 
+
